b932882c651c3cd915f56d395e1883bb05b42184
Kjartan Kristjansson <kjarisk@gmail.com>
2017-07-25 07:34:04 +0200

    Starting setup for boilerplate

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -1,22 +1,19 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import compression from 'compression';
 
 /* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath,
-}));
+app.use(compression());
+app.use(express.static('dist'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../src/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
 
+/*
 app.get('/users', (req, res) => {
   // Hard coding for simplicity. This is your database
   res.json([
@@ -25,6 +22,7 @@ app.get('/users', (req, res) => {
     {"id": 3, "firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"},
   ]);
 });
+*/
 
 app.listen(port, (err) => {
   if (err) {
